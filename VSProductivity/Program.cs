bea60a94f6b30a9e66f4197613ad7d9be424533b
Aleksandr Popov <popov@prg-it.ru>
2021-09-22 17:33:00 +0500

    t2

--- VSProductivity/Program.cs
+++ VSProductivity/Program.cs
@@ -20,7 +20,7 @@ namespace HotKeys
                 .ToList();           
 
             Console.ReadLine();
-            Console.WriteLine();
+            Console.WriteLine("234");
             Console.WriteLine();
             Console.WriteLine();
 
